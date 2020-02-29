e8b9fff857e4044edad41b6d8768380c5f9c0533
mglvc <m.golovatskikh@mail.ru>
2020-02-29 16:27:12 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,30 +12,51 @@ def start_message(message):
     markup = telebot.types.InlineKeyboardMarkup()
     markup.add(telebot.types.InlineKeyboardButton(text='Бакалавриат или специалитет', callback_data=3))
     markup.add(telebot.types.InlineKeyboardButton(text='Магистратуру', callback_data=4))
-    markup.add(telebot.types.InlineKeyboardButton(text='Почитать FAQ', callback_data=5))
+    #markup.add(telebot.types.InlineKeyboardButton(text='Почитать FAQ', callback_data=5))
     bot.send_message(message.chat.id, text="Вы хотите поступить в...", reply_markup=markup)
 
 @bot.callback_query_handler(func=lambda call: True)
 def query_handler(call):
     global bac, mag, phis, prmath, russ, inf
-    #bot.answer_callback_query(callback_query_id=call.id, text='Спасибо за честный ответ!')
-    #answer = ''
     if call.data == '3':
         answer = 'бакалавриат или специалитет'
         bac = 1
-        print(bac)
-        bot.send_message(call.message.chat.id, answer)
+        
+        keyboardmain = types.InlineKeyboardMarkup(row_width=4)
+        keyboardmain.add(types.InlineKeyboardButton(text="физику, проф. математику и русский", callback_data="11"))
+        keyboardmain.add(types.InlineKeyboardButton(text="физику, информатику, проф. математику и русский", callback_data="12"))
+        keyboardmain.add(types.InlineKeyboardButton(text="информатику, проф. математику, русский", callback_data="13"))
+        keyboardmain.add(telebot.types.InlineKeyboardButton(text='Почитать FAQ', callback_data=5))
+        bot.edit_message_text(chat_id=call.message.chat.id,message_id=call.message.message_id, text="вы собираетесь в бакалавриат или специалитет. Вы сдавали...",reply_markup=keyboardmain)
+
+    elif call.data == '11':
+    	phis, prmath, russ = 1, 1, 1
+    	bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
+    	bot.send_message(call.message.chat.id, "физику, профильную математику, русский")
+    
+    elif call.data == '12':
+    	phis, inf, prmath, russ = 1, 1, 1, 1
+    	bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
+    	bot.send_message(call.message.chat.id, "физику, профильную математику, информатику, русский")
+    
+    elif call.data == '13':
+    	inf, prmath, russ = 1, 1, 1
+    	bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
+    	bot.send_message(call.message.chat.id, "информатику, профильную математику, русский")
+
     elif call.data == '4':
         answer = 'магистратуру'
         mag = 1
-        bot.send_message(call.message.chat.id, answer)
+        key = types.InlineKeyboardMarkup(row_width=1)
+        key.add(types.InlineKeyboardButton(text="FAQ", callback_data="6"))
+        bot.edit_message_text(chat_id = call.message.chat.id, message_id=call.message.message_id, text= "вы собираетесь поступать в магистратуру", reply_markup=key)
+    
+    elif call.data == '6':   
+        bot.send_message(call.message.chat.id, "вы можете прочитать FAQ перейдя по этой ссылке https://pk.mipt.ru/master/question-answer/")    
+    
     elif call.data == '5':
         bot.send_message(call.message.chat.id, "вы можете прочитать FAQ перейдя по этой ссылке https://pk.mipt.ru/bachelor/question-answer/")
-    bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
-
-
-    	bot.send_message(message.chat.id, text="Вы сдавали...", reply_markup=markup)
-
+    #bot.edit_message_reply_markup(call.message.chat.id, call.message.message_id)
 
 
 @bot.message_handler(commands=['faq'])
@@ -44,8 +65,5 @@ def send_faq(message):
 		url_button = types.InlineKeyboardButton(text="Перейти на FAQ", url="https://pk.mipt.ru/bachelor/question-answer/")
 		keyboard.add(url_button)
 		bot.send_message(message.chat.id, "Нажми на кнопку", reply_markup=keyboard)
-
-#@bot.message_handler(content_types=['text'])
-#def send_text(message):
 
 bot.polling()
