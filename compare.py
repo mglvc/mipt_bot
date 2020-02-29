0999e019522630db26ce441bd151ef5de8dd27ad
VlatekSoft <34581167+VlatekSoft@users.noreply.github.com>
2020-03-01 00:04:29 +0300

    обернул в функцию

--- compare.py
+++ compare.py
@@ -6,30 +6,35 @@ df['answer'].str.lower()
 
 QuestionUser = "выдаются ли общежития на время поступления"
 
-print(len(QuestionForum))
-max = 0.0
-x = 0
-Answer,Answer2,Answer3,Answer4,Answer5 = '','','','',''
-Score,Score2,Score3,Score4,Score5 = 0,0,0,0,0
 
-for i in df['question']:
-    #print(i)
-    #print(df[x-1:x])
-    s = SequenceMatcher(lambda x: x==" ", QuestionUser, str(i)) 
-    ratio = s.ratio()
-    if ratio > max: 
-        Answer5, Score5 = Answer4, Score4
-        Answer4, Score4 = Answer3, Score3
-        Answer3, Score3 = Answer2, Score2
-        Answer2, Score2 = Answer, Score
-        Answer = str(i)
-        Score = ratio
-        max = ratio
-        rawdata = [df[x:x+1],x]
-        
-   
-    x+=1
+
+def compare():
+    global Answer,Answer2,Answer3,Answer4,Answer5,Score,Score2,Score3,Score4,Score5, rawdata
+    maxim = 0
+    x = 0
+    Answer,Answer2,Answer3,Answer4,Answer5 = '','','','',''
+    Score,Score2,Score3,Score4,Score5 = 0,0,0,0,0
+    for i in df['question']:
+        #print(i)
+        #print(df[x-1:x])
+        s = SequenceMatcher(lambda x: x==" ", QuestionUser, str(i)) 
+        ratio = s.ratio()
+        if ratio > maxim: 
+            Answer5, Score5 = Answer4, Score4
+            Answer4, Score4 = Answer3, Score3
+            Answer3, Score3 = Answer2, Score2
+            Answer2, Score2 = Answer, Score
+            Answer = str(i)
+            Score = ratio
+            maxim = ratio
+            rawdata = [df[x:x+1],x]
+            
     
+        x+=1
+    result = [Answer, Score,  Answer2, Score2,  Answer3, Score3,  Answer4, Score4,  Answer5, Score5,  rawdata]
+    return result
+
+compare()    
 print(Answer, Score, '\n', Answer2, Score2, '\n', Answer3, Score3, '\n', Answer4, Score4, '\n', Answer5, Score5, '\n', rawdata, '\n')
 
 
