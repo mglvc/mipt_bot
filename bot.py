ae5d28b568de1110a8109e47b2548cf930864c5a
mglvc <m.golovatskikh@mail.ru>
2020-02-29 12:36:36 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,13 @@
+import telebot
+import time
+import datetime
+
+TOKEN = '955620028:AAFuqC8MSVkQa-50OdCnCxNyI5BJXHCAf8c'
+
+phis, inf, russ, math = 0, 0, 0, 0
+
+bot = telebot.TeleBot(TOKEN)
+
+
+
+bot.polling()
