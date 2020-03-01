dc97aa7b6eacf48ae7df4c98c5e8ec0f27c6046a
Maxim Sinyaev <bemaradio@gmail.com>
2020-03-02 03:24:52 +0700

    configs deleted, it was a bad idea to publish them

--- config/aws_config.py
+++
@@ -1,1 +0,0 @@
-AWS_URL = 'https://0e1tig43bl.execute-api.us-east-1.amazonaws.com/beta/mipt_bot'
--- config/rds_config.py
+++
@@ -1,4 +0,0 @@
-db_username = "admin"
-db_password = "d5<?HNFG"
-db_name = "ExampleDB"
-db_endpoint = "mysqlforlambdatest.chhuo1gywxj5.us-east-1.rds.amazonaws.com"
